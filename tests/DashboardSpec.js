2a98451b64aaa5c59e92d63a2e34fdbbf35a560e
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-01-13 21:06:36 +0300

    Set up unit tests

---
+++ tests/DashboardSpec.js
@@ -0,0 +1,30 @@
+/**
+ * Dashboard module unit tests
+ * 
+ */
+
+//@TODO: I thought this is automatically loaded by the webpack entry point
+require('../src/main.js');
+
+var DashboardView = require('../src/views/dashboard_view.js');
+
+
+    
+describe('Dashboard Module', function () {
+    var dashboardView;
+    var d = document.querySelector('.icon-display');
+    
+    //Initialize the dashboardView
+    beforeAll(function () {
+         dashboardView = new DashboardView();
+         dashboardView.render();
+    });
+    
+    beforeEach(function () {
+        
+    });
+    
+    it("should load the dashboard icons", function() {
+         expect(d.nodeName).toBe('DIV');
+    });
+});
